23bfd8cbaef16f9ca94f62ae0320d3bde5b7200b
Shawn Radebe <radebeshawnn@gmail.com>
2021-11-09 00:51:44 +0200

    setup for heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const PORT = 3100
+const PORT = process.env.PORT || 3100
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 dotenv.config()
